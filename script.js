85004831bdb59bb759a96eb0dbbf56e241b3bcce
Matt Charles <matthewcharles90@gmail.com>
2022-07-07 11:48:58 -0400

    async

--- script.js
+++ script.js
@@ -12,5 +12,7 @@ function addGenre() {
 
     levels.forEach(level => {
         fetch(`https://opentdb.com/api.php?amount=1&category=11&difficulty=${level}&type=boolean`)
+        .then(response => response.json())
+        .then(data => console.log(data))
     })
 }
