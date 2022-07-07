39c59d1e7c4712e79d2fd7b8896841fa001ff3ce
Matt Charles <matthewcharles90@gmail.com>
2022-07-07 13:07:23 -0400

    Add difficulty level
    if statements

--- script.js
+++ script.js
@@ -11,15 +11,29 @@ function addGenre() {
     game.append(column)
 
     levels.forEach(level => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        column.append(card)
+
+        if (level === 'easey') {
+            card.innerHTML = 100
+        }
+        if (level === 'medium') {
+            card.innerHTML = 200
+        }
+        if (level === 'hard'){
+            card.innerHTML = 300
+        }
+
         fetch(`https://opentdb.com/api.php?amount=1&category=11&difficulty=${level}&type=boolean`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
             card.setAttribute('data-question', data.results[0].question)
             card.setAttribute('data-answer', data.results[0].correct_answer)
-
         })
     })
+
 }
 
 addGenre()
